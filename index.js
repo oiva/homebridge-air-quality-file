b366fd81ee0332ee126c3ec2e18fd205ff7fe37c
Oiva Eskola <oiva.eskola@gmail.com>
2020-03-13 08:38:42 +0200

    add support for reporting pm2.5, pm10, and air quality index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
-var Service, Characteristic;
+var Service, Characteristic, AirQuality;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
+  AirQuality = Characteristic.AirQuality;
 
   homebridge.registerAccessory("homebridge-air-quality-file", "AirQualityFile", AirQualityFileAccessory);
 }
@@ -13,24 +14,63 @@ function AirQualityFileAccessory(log, config) {
   this.name = config["name"];
   this.filePath = config["file_path"];
 
-  this.service = new Service.AirQualitySensor(this.name);
-
-  this.service
-    .getCharacteristic(Characteristic.AirQuality)
-    .on('get', this.getState.bind(this));
+  var airQualityIndexName = config.airQualityIndexName || 'Air Quality';
+  this.sensor = new Service.AirQualitySensor(airQualityIndexName);
 }
 
-AirQualityFileAccessory.prototype.getState = function(callback) {
-  fs.readFile(this.filePath, 'utf8', function(err, data) {
+function readFile(filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, data) {
     if (err) {
-      callback(err);
-      return
+      return callback(err)
     }
-
-    callback(null, parseFloat(data))
+    var readings = JSON.parse(data)
+    if (readings.length == 0) {
+        return callback(null, null)
+    }
+    var latest = readings[readings.length - 1]
+    return callback(null, latest)
   })
 }
 
-AirQualityFileAccessory.prototype.getServices = function() {
-  return [this.service];
+AirQualityFileAccessory.prototype = {
+  getAirQuality: function(density) {
+    if (density === null) {
+      return AirQuality.UNKNOWN
+    }
+    if (density <= 25) {
+      return AirQuality.EXCELLENT
+    }
+    if (density <= 50) {
+      return AirQuality.GOOD
+    }
+    if (density <= 100) {
+      return AirQuality.FAIR
+    }
+    if (density <= 150) {
+      return AirQuality.INFERIOR
+    }
+    return AirQuality.POOR
+  },
+
+  updateAirQualityIndex: function (callback) {
+    readFile(this.filePath, function(err, data) {
+      if (err || data === null) {
+        this.sensor.setCharacteristic(Characteristic.StatusFault, 1);
+        callback(err);
+        return
+      }
+      var pm25 = parseFloat(data.pm25)
+      var pm10 = parseFloat(data.pm10)
+      var aiq = this.getAirQuality(pm10)
+      this.sensor.setCharacteristic(Characteristic.PM2_5Density, pm25)
+      this.sensor.setCharacteristic(Characteristic.PM10Density, pm10)
+      this.sensor.setCharacteristic(Characteristic.AirQuality, aiq)
+      callback(null, aiq)
+    }.bind(this))
+  },
+
+  getServices: function() {
+    this.sensor.getCharacteristic(Characteristic.AirQuality).on('get', this.updateAirQualityIndex.bind(this))
+    return [this.sensor]
+  }
 }
