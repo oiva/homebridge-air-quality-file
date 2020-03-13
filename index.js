890690d94e43252f9a54f00216b23c5e51e39507
Oiva Eskola <oiva.eskola@gmail.com>
2020-03-13 08:49:47 +0200

    use prettier for formatting

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-var fs = require('fs');
-var Service, Characteristic, AirQuality;
+var fs = require('fs')
+var Service, Characteristic, AirQuality
 
 module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  AirQuality = Characteristic.AirQuality;
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  AirQuality = Characteristic.AirQuality
 
-  homebridge.registerAccessory("homebridge-air-quality-file", "AirQualityFile", AirQualityFileAccessory);
+  homebridge.registerAccessory(
+    'homebridge-air-quality-file',
+    'AirQualityFile',
+    AirQualityFileAccessory
+  )
 }
 
 function AirQualityFileAccessory(log, config) {
-  this.log = log;
-  this.name = config["name"];
-  this.filePath = config["file_path"];
+  this.log = log
+  this.name = config['name']
+  this.filePath = config['file_path']
 
-  var airQualityIndexName = config.airQualityIndexName || 'Air Quality';
-  this.sensor = new Service.AirQualitySensor(airQualityIndexName);
+  var airQualityIndexName = config.airQualityIndexName || 'Air Quality'
+  this.sensor = new Service.AirQualitySensor(airQualityIndexName)
 }
 
 function readFile(filePath, callback) {
@@ -25,7 +29,7 @@ function readFile(filePath, callback) {
     }
     var readings = JSON.parse(data)
     if (readings.length == 0) {
-        return callback(null, null)
+      return callback(null, null)
     }
     var latest = readings[readings.length - 1]
     return callback(null, latest)
@@ -52,25 +56,30 @@ AirQualityFileAccessory.prototype = {
     return AirQuality.POOR
   },
 
-  updateAirQualityIndex: function (callback) {
-    readFile(this.filePath, function(err, data) {
-      if (err || data === null) {
-        this.sensor.setCharacteristic(Characteristic.StatusFault, 1);
-        callback(err);
-        return
-      }
-      var pm25 = parseFloat(data.pm25)
-      var pm10 = parseFloat(data.pm10)
-      var aiq = this.getAirQuality(pm10)
-      this.sensor.setCharacteristic(Characteristic.PM2_5Density, pm25)
-      this.sensor.setCharacteristic(Characteristic.PM10Density, pm10)
-      this.sensor.setCharacteristic(Characteristic.AirQuality, aiq)
-      callback(null, aiq)
-    }.bind(this))
+  updateAirQualityIndex: function(callback) {
+    readFile(
+      this.filePath,
+      function(err, data) {
+        if (err || data === null) {
+          this.sensor.setCharacteristic(Characteristic.StatusFault, 1)
+          callback(err)
+          return
+        }
+        var pm25 = parseFloat(data.pm25)
+        var pm10 = parseFloat(data.pm10)
+        var aiq = this.getAirQuality(pm10)
+        this.sensor.setCharacteristic(Characteristic.PM2_5Density, pm25)
+        this.sensor.setCharacteristic(Characteristic.PM10Density, pm10)
+        this.sensor.setCharacteristic(Characteristic.AirQuality, aiq)
+        callback(null, aiq)
+      }.bind(this)
+    )
   },
 
   getServices: function() {
-    this.sensor.getCharacteristic(Characteristic.AirQuality).on('get', this.updateAirQualityIndex.bind(this))
+    this.sensor
+      .getCharacteristic(Characteristic.AirQuality)
+      .on('get', this.updateAirQualityIndex.bind(this))
     return [this.sensor]
   }
 }
